7ae1448dde0ab6c5c6698ba115c46fa633834ff5
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-12-12 08:37:36 +0530

    Get API Code Refactor In-progress

--- src/test/java/qa/wiremock/concepts/MockGetAPITest.java
+++ src/test/java/qa/wiremock/concepts/MockGetAPITest.java
@@ -14,49 +14,49 @@ import static io.restassured.RestAssured.given;
 public class MockGetAPITest {
 
     private static final String HOST = "localhost";
-
     private static final int PORT = 8080;
-
     public static WireMockServer wireMockServer;
 
     @BeforeTest
     public void startupServer() {
         wireMockServer = new WireMockServer(PORT);
-        wireMockServer.start();
-        WireMock.configureFor(HOST, PORT);
-        ResponseDefinitionBuilder responseDefinitionBuilder = new ResponseDefinitionBuilder();
-        responseDefinitionBuilder.withStatus(200);
-        responseDefinitionBuilder.withHeader("Content-Type", "application/json");
-        responseDefinitionBuilder.withBodyFile("json/get_user.json");
-        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/user/emp101")).willReturn(responseDefinitionBuilder));
+        try {
+            wireMockServer.start();
+            WireMock.configureFor(HOST, PORT);
+            ResponseDefinitionBuilder responseDefinitionBuilder = new ResponseDefinitionBuilder();
+            responseDefinitionBuilder.withStatus(200);
+            responseDefinitionBuilder.withHeader("Content-Type", "application/json");
+            responseDefinitionBuilder.withBodyFile("json/get_user.json");
+            WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/user/emp101")).willReturn(responseDefinitionBuilder));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterTest
     public void shutdownServer() {
-        if (wireMockServer.isRunning() && null != wireMockServer) {
+        if (wireMockServer != null && wireMockServer.isRunning()) {
             wireMockServer.shutdownServer();
         }
     }
 
     @Test
     public void mockGetApiTest() {
+        String requestUrl = String.format("http://%s:%d/user/emp101", HOST, PORT);
+
         ValidatableResponse response =
                 given()
                 .when()
-                        .get("http://localhost:8080/user/emp101")
+                        .get(requestUrl)
                 .then()
                         .assertThat()
                         .statusCode(200)
                         .log()
                         .all();
 
-        Assert.assertEquals(response.extract().statusCode(), 200);
-        Assert.assertEquals(response.extract().header("Content-Type"), "application/json");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.id"), "EMP101");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.name"), "John Doe");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.address.city"), "New York");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.address.country"), "United States");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.retrievedAt"), "2023-11-04T03:48:52.454Z");
+        Assert.assertEquals(response.extract().statusCode(), 200, "Unexpected status code");
+        Assert.assertEquals(response.extract().header("Content-Type"), "application/json", "Unexpected Content-Type");
+        Assert.assertEquals(response.extract().jsonPath().get("worker.id"), "EMP101", "Incorrect worker ID");
     }
 
 }
