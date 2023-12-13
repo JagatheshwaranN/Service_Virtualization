ed5a248e8f626e91d76affac31c9cb4a3a43825f
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-12-13 13:51:41 +0530

    Verify GET and POST API hits Scenario Update

--- src/test/java/qa/wiremock/concepts/VerifyMockGetAPITest.java
+++ src/test/java/qa/wiremock/concepts/VerifyMockGetAPITest.java
@@ -11,50 +11,99 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
+/**
+ * The code aims to simulate and test the behavior of a GET API endpoint
+ * ("/user/emp101") by mocking its response.
+ * It sets up a WireMock server and defines a stub for the specified API
+ * endpoint.
+ * Subsequently, it sends a request to this mocked endpoint and verifies
+ * whether the response matches the expected behavior.
+ * It also ensures that only one GET request was made to the URL "/user/emp101"
+ * within the scope of the test.
+ * This helps to confirm that the test is behaving as intended and that there
+ * are no unexpected interactions with the mocked API.
+ *
+ * @author Jagatheshwaran N
+ */
 public class VerifyMockGetAPITest {
 
+    // Constants for host and port
     private static final String HOST = "localhost";
-
     private static final int PORT = 8080;
 
+    // Instance of WireMockServer
     public static WireMockServer wireMockServer;
 
+    // Method to start the WireMock server and configure stubs before test execution
     @BeforeTest
     public void startupServer() {
+        // Creates a new WireMock server instance.
         wireMockServer = new WireMockServer(PORT);
+
+        // Starts the WireMock server.
         wireMockServer.start();
+
+        // Configures WireMock to listen on the specified host and port.
         WireMock.configureFor(HOST, PORT);
+
+        // Creates a response definition builder object.
         ResponseDefinitionBuilder responseDefinitionBuilder = new ResponseDefinitionBuilder();
+
+        // Sets the response status code to 200 (OK).
         responseDefinitionBuilder.withStatus(200);
+
+        // Sets the response content type to "application/json".
         responseDefinitionBuilder.withHeader("Content-Type", "application/json");
+
+        // Defines the response body using a JSON file located in "json/get_user.json".
         responseDefinitionBuilder.withBodyFile("json/get_user.json");
-        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/user/emp101")).willReturn(responseDefinitionBuilder));
+
+        // Stubs a response for the GET request to "/user/emp101".
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/user/emp101"))
+                .willReturn(responseDefinitionBuilder));
     }
 
+    // Method to shut down the WireMock server after test execution
     @AfterTest
     public void shutdownServer() {
-        if (wireMockServer.isRunning() && null != wireMockServer) {
+        // Checks if the WireMock server is running and not null before shutting it down.
+        if (wireMockServer != null && wireMockServer.isRunning()) {
             wireMockServer.shutdownServer();
         }
     }
 
+    // Test method to verify the behavior of the mocked GET API
     @Test
-    public void verifyMockGetApiTest() {
-        ValidatableResponse response =
-                given()
-                .when()
-                        .get("http://localhost:8080/user/emp101")
-                .then()
-                        .assertThat()
-                        .statusCode(200)
-                        .log()
-                        .all();
+    public void testVerifyMockGetAPI() {
+        // Construct the request URL using the HOST and PORT constants
+        String requestUrl = String.format("http://%s:%d/user/emp101", HOST, PORT);
 
+        // Perform the GET request and validate the response
+        ValidatableResponse response =
+                given() // Start building the request specification
+                .when() // Perform the action (in this case, an HTTP GET request)
+                        .get(requestUrl) // Specify the URL to send the GET request
+                .then() // Start defining assertions on the response
+                        .assertThat() // Begin assertion configuration
+                        .statusCode(200) // Check that the response status code is 200 (OK)
+                        .log() // Log details of the request and response
+                        .all(); // Log all details (request headers, body, response headers, and body)
+
+        // Verifying that the GET request was made to the specified endpoint once and
+        // received the expected request once.
         WireMock.verify(1, WireMock.getRequestedFor(WireMock.urlPathEqualTo("/user/emp101")));
 
-        Assert.assertEquals(response.extract().statusCode(), 200);
-        Assert.assertEquals(response.extract().header("Content-Type"), "application/json");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.id"), "EMP101");
+        // Assertions to validate specific fields in the response
+        // Verify the status code
+        Assert.assertEquals(response.extract().statusCode(), 200, "Unexpected status code");
+
+        // Verify the Content-Type header
+        Assert.assertEquals(response.extract().header("Content-Type"), "application/json", "Unexpected Content-Type");
+
+        // Verify the worker ID
+        Assert.assertEquals(response.extract().jsonPath().get("worker.id"), "EMP101", "Incorrect worker ID");
+
+        // Verifies additional JSON values using JSON Path assertions.
         Assert.assertEquals(response.extract().jsonPath().get("worker.name"), "John Doe");
         Assert.assertEquals(response.extract().jsonPath().get("worker.address.city"), "New York");
         Assert.assertEquals(response.extract().jsonPath().get("worker.address.country"), "United States");
--- src/test/java/qa/wiremock/concepts/VerifyMockPostAPITest.java
+++ src/test/java/qa/wiremock/concepts/VerifyMockPostAPITest.java
@@ -11,79 +11,114 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
+/**
+ * The code focuses on simulating and testing the behavior of a POST API endpoint
+ * ("/user/add") by mocking its response.
+ * It sets up a WireMock server, defines a stub for the specified API endpoint,
+ * sends a request to this mocked endpoint, and verifies whether the response matches
+ * the expected behavior.
+ * It also ensures that only one POST request was made to the URL "/user/add"
+ * within the scope of the test.
+ * This helps to confirm that the test is behaving as intended and that there
+ * are no unexpected interactions with the mocked API.
+ *
+ * @author Jagatheshwaran N
+ */
 public class VerifyMockPostAPITest {
 
+    // Constants for host and port
     private static final String HOST = "localhost";
-
     private static final int PORT = 8080;
 
+    // Instance of WireMockServer
     public static WireMockServer wireMockServer;
 
     @BeforeTest
     public void startupServer() {
+        // Initialize and start WireMockServer
         wireMockServer = new WireMockServer(PORT);
+
+        // Start the WireMockServer
         wireMockServer.start();
+
+        // Configure WireMock for host and port
         WireMock.configureFor(HOST, PORT);
+
+        // Define the expected response for the POST /user/add endpoint
         ResponseDefinitionBuilder responseDefinitionBuilder = new ResponseDefinitionBuilder();
+
+        // Set the expected status code
         responseDefinitionBuilder.withStatus(201);
+
+        // Set the expected Content-Type header
         responseDefinitionBuilder.withHeader("Content-Type", "application/json");
+
+        // Set the response body file
         responseDefinitionBuilder.withBodyFile("json/add_user.json");
-        WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/user/add")).withRequestBody(WireMock.equalToJson("""
-                {
-                  "name": "John Doe",
-                  "location": "New York",
-                  "phone": "123-456-7890",
-                  "address": {
-                    "city": "New York",
-                    "state": "New York",
-                    "zipcode": "10001",
-                    "country": "United States"
-                  }
-                }""")).willReturn(responseDefinitionBuilder));
+
+        // Stub the WireMock behavior for the POST request with expected payload
+        WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/user/add"))
+                .withRequestBody(WireMock.equalToJson(getPayload())) // Verify the request body matches the expected payload
+                .willReturn(responseDefinitionBuilder));
+
     }
 
+    // Method to shut down WireMockServer after tests
     @AfterTest
     public void shutdownServer() {
-        if (wireMockServer.isRunning() && null != wireMockServer) {
+        // Check if WireMockServer is initialized and running, then shut it down
+        if (wireMockServer != null && wireMockServer.isRunning()) {
             wireMockServer.shutdownServer();
         }
     }
 
     @Test
-    public void mockPostApiTest() {
+    public void testVerifyMockPostAPI() {
+        // Construct the request URL using the HOST and PORT constants
+        String requestUrl = String.format("http://%s:%d/user/add", HOST, PORT);
 
-        String payloadJson = """
-                                {
-                                  "name": "John Doe",
-                                  "location": "New York",
-                                  "phone": "123-456-7890",
-                                  "address": {
-                                    "city": "New York",
-                                    "state": "New York",
-                                    "zipcode": "10001",
-                                    "country": "United States"
-                                  }
-                                }""";
+        // Get the JSON payload for the request
+        String payloadJson = getPayload();
+
+        // Perform the POST request and validate the response
         ValidatableResponse response =
-                given()
-                        .body(payloadJson)
-                .when()
-                        .post("http://localhost:8080/user/add")
-                .then()
-                        .assertThat()
-                        .statusCode(201)
-                        .log()
-                        .all();
+                given() // Start building the request specification
+                        .body(payloadJson) // Set the request body to the provided JSON payload
+                .when() // Perform the action (in this case, an HTTP POST request)
+                        .post(requestUrl) // Specify the URL to send the POST request
+                .then() // Start defining assertions on the response
+                        .assertThat() // Begin assertion configuration
+                        .statusCode(201) // Check that the response status code is 201 (Created)
+                        .log() // Log details of the request and response
+                        .all(); // Log all details (request headers, body, response headers, and body)
 
-        Assert.assertEquals(response.extract().statusCode(), 201);
-        Assert.assertEquals(response.extract().header("Content-Type"), "application/json");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.id"), "EMP101");
-        Assert.assertEquals(response.extract().jsonPath().get("worker.name"), "John Doe");
+        // Verifying that the POST request was made to the specified endpoint once and
+        // received the expected request once.
+        WireMock.verify(1, WireMock.postRequestedFor(WireMock.urlEqualTo("/user/add")).withRequestBody(WireMock.equalToJson(
+                getPayload())));
+
+        // Assertions to validate specific fields in the response
+        // Verify the status code
+        Assert.assertEquals(201, response.extract().statusCode(), "Unexpected status code");
+
+        // Verify the Content-Type header
+        Assert.assertEquals(response.extract().header("Content-Type"),"application/json", "Unexpected Content-Type");
+
+        // Verify the worker ID
+        Assert.assertEquals(response.extract().jsonPath().get("worker.id"), "EMP101", "Incorrect worker ID");
+
+        // Verify the worker name
+        Assert.assertEquals(response.extract().jsonPath().get("worker.name"), "John Doe", "Incorrect worker name");
+
+        // Verifies additional JSON values using JSON Path assertions.
         Assert.assertEquals(response.extract().jsonPath().get("worker.address.city"), "New York");
         Assert.assertEquals(response.extract().jsonPath().get("worker.address.country"), "United States");
         Assert.assertEquals(response.extract().jsonPath().get("worker.createdAt"), "2023-11-04T02:48:52.454Z");
+    }
 
-        WireMock.verify(1, WireMock.postRequestedFor(WireMock.urlEqualTo("/user/add")).withRequestBody(WireMock.equalToJson("""
+    // Method to return the JSON payload for the POST request
+    private String getPayload() {
+        return """
                 {
                   "name": "John Doe",
                   "location": "New York",
@@ -94,7 +129,7 @@ public class VerifyMockPostAPITest {
                     "zipcode": "10001",
                     "country": "United States"
                   }
-                }""")));
+                }""";
     }
 
 }
