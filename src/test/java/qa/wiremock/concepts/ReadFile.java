aa07c763e64c254526ef4848c6caa9d2a3458f72
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-12-14 17:06:41 +0530

    Proxy Scenario Description Update

--- src/test/java/qa/wiremock/concepts/ReadFile.java
+++ src/test/java/qa/wiremock/concepts/ReadFile.java
@@ -13,7 +13,6 @@ public class ReadFile {
         JSONObject emptyCartJson = new JSONObject(readJsonFile(System.getProperty("user.dir")+"/src/test/resources/__files/json/emptycart.json"));
         System.out.println(emptyCartJson);
     }
-
     private static String readJsonFile(String fileName) throws IOException {
         Path path = Path.of(fileName);
         return Files.readString(path, StandardCharsets.UTF_8);
